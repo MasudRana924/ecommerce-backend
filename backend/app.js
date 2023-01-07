fe9c4656a076e28cba8667a767ce0983df831405
Masud Rana <masud15-924@diu.edu.bd>
2023-01-07 09:35:30 +0600

    first api done

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,12 @@
 const express = require("express");
 const app = express();
+app.use(express.json());
+
+
+
+
+const product=require("./routes/productRouter");
+//route gula import 
+
+app.use("/api/v1", product);
 module.exports = app;
